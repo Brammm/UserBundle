64e44ca9866d804ec3f239ec358ac191aba01499
Bram Van der Sype <bvandersype@moovly.com>
2013-11-15 10:15:37 +0100

    -n Some security work being done

---
+++ BrammmUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brammm\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BrammmUserBundle extends Bundle
+{
+}
---
+++ DependencyInjection/BrammmUserExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Brammm\UserBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class BrammmUserExtension extends Extension
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+} 
