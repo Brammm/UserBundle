246f1327a31bb5e7969525635342f83ed5d24372
Bram Van der Sype <bvandersype@moovly.com>
2014-02-10 15:15:52 +0100

    -n test

---
+++ Tests/EventListener/CanonicalizerSubscriberTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Brammm\UserBundle\Tests\EventListener;
+
+use Brammm\UserBundle\Entity\User;
+use Brammm\UserBundle\EventListener\CanonicalizerSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+
+class CanonicalizerSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanonicalize()
+    {
+        $canonicalizerMock = $this->getMockBuilder('\Brammm\UserBundle\Services\Canonicalizer')
+            ->getMock()
+            ->expects($this->once())
+            ->method('canonicalize')
+            ->with($this->equalTo('foo@bar.com'));
+
+        $user = new User();
+        $user->setEmail('foo@bar.com');
+
+        $eventMock = $this->getMockBuilder('\Doctrine\Common\Persistence\Event\LifecycleEventArgs')
+            ->disableOriginalConstructor()
+            ->getMock()
+            ->expects($this->once())
+            ->method('getObject')
+            ->will($this->returnValue($user));
+
+        $subscriber = new CanonicalizerSubscriber($canonicalizerMock);
+
+        $subscriber->prePersist($eventMock);
+    }
+} 
